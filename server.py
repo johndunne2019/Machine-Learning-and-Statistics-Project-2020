b36cefdf772f82a48d013f9bf8b96c36984ae1a3
John Dunne <G00273895@gmit.ie>
2021-01-02 19:03:33 +0000

    server.py - changed the function at the app route for predictions to take an integer as input and return back a predicted power output response to the input. Tested on local host and with curl calls and working good

--- server.py
+++ server.py
@@ -4,10 +4,12 @@
 # Adapted from lecture series example - https://github.com/ianmcloughlin/random-app/blob/master/rando.py
 
 # flask for web app.
-import flask as fl
-#from flask import Flask
+#import flask as fl
+from flask import Flask, jsonify, request, abort
 
 import numpy as np
+
+import codecs, json 
 
 # import keras and load the saved model
 # tensorflow documentation on saving and loading a model - https://www.tensorflow.org/guide/keras/save_and_serialize
@@ -18,8 +20,8 @@ model = keras.models.load_model("model.h5")
 print(model.predict([25]))
 
 # Create a new web app.
-app = fl.Flask(__name__)
-#app = Flask(__name__, static_url_path='', static_folder='static')
+#app = fl.Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 # Add root route.
 # This will serve out the index.html page at the root
@@ -27,19 +29,21 @@ app = fl.Flask(__name__)
 def home():
   return app.send_static_file('index.html')
 
-# Add root to return power value for wind speed entered by user
-# object of type ndarray is not JSON serializable error was returned at first, I added new return statement with prediction returned as a string
-@app.route('/predict')
-def predict():
-  p = model.predict([25])
+# Add route to return power value for wind speed entered by user
+# tested and returning power output successfully at app route with response 200 - http://127.0.0.1:5000/predict/10
+
+# tested with curl call below
+# curl "http://127.0.0.1:5000/predict/20
+# {"predicted power output":"98.23132"}
+
+@app.route('/predict/<int:id>')
+def predict(id):
+  p = model.predict([id])
+  # object of type ndarray is not JSON serializable error was returned at first, I added new return statement with prediction returned as a string
   return {"predicted power output": str(p[0][0])}
 
-
 # from stack overflow - https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
- # prediction = model.predict(np.array(25).tolist()).tolist()
- # return jsonify({'prediction': prediction})
 
-# run the app when script is run from command line
-# debug mode is on, the server will reload if any changes made to the file 
+# debug mode turned on for testing purposes, commented out in final production version
 #if __name__ == '__main__' :
    #app.run(debug= True)
