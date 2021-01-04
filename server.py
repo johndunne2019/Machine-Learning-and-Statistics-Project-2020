542cbadc1d7f880b9a02acef87f17031e7e05105
John Dunne <G00273895@gmit.ie>
2021-01-04 12:39:48 +0000

    Turned off debug mode in server.py file and added comments, server is finished

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 # Machine learning and statistics project
 # John Dunne G00273895
-# flask server to be run in a virtual environment 
+# flask server to be run in a virtual environment or on local machine
 # Adapted from lecture series example - https://github.com/ianmcloughlin/random-app/blob/master/rando.py
+# To run on windows machine:
+# set FLASK_APP=server.py
+# python -m flask run
+# when server is running access on web browser at - http://127.0.0.1:5000/
 
-# flask for web app.
-#import flask as fl
+# import flask for web app.
+#import flask as fl - alternative way to import flask
 from flask import Flask
 
 # import keras and load the saved model
@@ -12,12 +16,8 @@ from flask import Flask
 from tensorflow import keras
 model = keras.models.load_model("model.h5")
 
-# test the model is returning a prediction 
-#print(model.predict([25]))
-#print(model.predict([25]).tolist())
-
 # Create a new web app.
-#app = fl.Flask(__name__)
+#app = fl.Flask(__name__)  - alternative way to create a new app
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 # Add root route.
@@ -26,19 +26,19 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def home():
   return app.send_static_file('index.html')
 
-# Add route to return power value for wind speed entered by user
+# Add route to return power value for wind speed entered by user with a GET method
 # tested and returning power output successfully at app route with response 200 - http://127.0.0.1:5000/predict/10
 # tested with curl call below
 # curl "http://127.0.0.1:5000/predict/20
+# object of type ndarray is not JSON serializable error was returned at first, I added new return statement with prediction returned as a string
+# from stack overflow - https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
 @app.route('/predict/<int:x>', methods=['GET'])
 def predict(x):
   prediction = model.predict([x])
-  # object of type ndarray is not JSON serializable error was returned at first, I added new return statement with prediction returned as a string
   #return {"predicted power output": str(prediction[0][0])}
   return str(prediction[0][0])
 
-# from stack overflow - https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
 
 # debug mode turned on for testing purposes, commented out in final production version
-if __name__ == '__main__' :
-  app.run(debug= True)
+#if __name__ == '__main__' :
+  #app.run(debug= True)
